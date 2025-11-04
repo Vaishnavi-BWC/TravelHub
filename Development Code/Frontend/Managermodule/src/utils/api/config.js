ee89582e31b531b13f747684dbc9f143b95b693e
Vaishnavi-BWC <vhirulkarbrainwave@gmail.com>
2025-11-04 10:11:42 +0530

    bug fixing

--- Development Code/Frontend/Managermodule/src/utils/api/config.js
+++
@@ -1,8 +0,0 @@
-// utils/api/config.js
-export const API_CONFIG = {
-  baseURL: 'http://bwc-97.brainwaveconsulting.co.in:8090',
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  timeout: 30000
-}
